f3578ddec5e50fbf94aa8a553d887bf09f44155c
R Sánchez <rene.as.himself@gmail.com>
2019-11-29 12:05:43 -0600

    NPM scripts and formatting

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -22,28 +22,7 @@ const ProjectsPage = () => (
       elementum ornare.
     </p>
 
-    <p>
-      Quisque pharetra suscipit tortor, quis ultricies lectus bibendum aliquet. Suspendisse vel
-      tincidunt ex. Vestibulum est odio, laoreet et eros at, aliquam pretium sem. Vestibulum
-      ullamcorper quis eros a rutrum. Duis porttitor, tortor ac egestas vehicula, orci leo blandit
-      lorem, scelerisque dignissim dui nunc id quam. Cras et hendrerit purus. Etiam viverra lacus
-      quis sapien euismod fermentum. Duis tortor lorem, iaculis placerat ante ut, placerat commodo
-      dolor. Morbi accumsan posuere purus, ac sodales lacus. Donec quam elit, scelerisque non mauris
-      nec, finibus semper est. Quisque auctor accumsan sodales. Fusce vulputate ornare mauris, sed
-      consectetur sem feugiat a.
-    </p>
-
-    <p>
-      Vestibulum ante erat, porta at odio ut, convallis tempor tortor. Quisque lacinia dolor at
-      convallis mattis. Donec vitae rutrum mauris. Aliquam eget euismod ante, non molestie nibh.
-      Quisque dictum nisi mauris, quis fermentum sem tincidunt ut. Praesent pharetra at nunc non
-      dapibus. Integer finibus sit amet tortor a viverra. Proin ligula augue, vestibulum et nisi
-      hendrerit, imperdiet iaculis odio. In hac habitasse platea dictumst. Duis sit amet tempor
-      orci, nec luctus diam. Donec eu est ut dolor dictum ornare sit amet sed arcu. Quisque
-      pellentesque risus enim, quis egestas quam tempus sit amet.
-    </p>
-
-    <Projects />
+    {/* <Projects /> */}
   </Layout>
 )
 
