3a338c3760c6f965ad4051a419dcc0c5ea62b9b2
R Sánchez <rene.as.himself@gmail.com>
2019-11-29 10:17:27 -0600

    Fixed GQL queries on index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,85 +1,83 @@
 import React from 'react'
 import { graphql } from 'gatsby'
 import styled from 'styled-components'
-import Layout from '../components/layout'
-import Projects from '../components/projects'
-import SEO from '../components/seo'
-import '../components/styles.scss'
-import Featured from '../components/featured'
-import Contact from '../components/contact'
-import BasicInfo from '../components/basic-info'
+import Layout from '../components/Layout'
+import Projects from '../components/Projects'
+import SEO from '../components/SEO'
+import '../static/css/styles.scss'
+import Featured from '../components/Featured'
+import Contact from '../components/Contact'
+import BasicInfo from '../components/BasicInfo'
 
-const SectionStyles = styled.section`
-    text-align: center;
+const ArticleStyles = styled.article`
+  text-align: center;
 `
 
 const IndexPage = ({ data }) => {
-    const { edges: projectImgData } = data.ProjectImgs
-    const { edges: featuredImgData } = data.FeaturedProjectImgs
+  const { edges: projectImgData } = data.ProjectImgs
+  const { edges: featuredImgData } = data.FeaturedProjectImgs
 
-    return (
-        <Layout>
-            <SEO
-                title='index'
-                description={data.description}
-                keywords={[`blog`, `technology`, `react`]}
-            />
+  return (
+    <Layout>
+      <SEO
+        title='index'
+        description={data.description}
+        keywords={[`blog`, `technology`, `react`]}
+      />
 
-            <SectionStyles>
-                <BasicInfo />
-                {/* <div href='#projects' style={{ height: '100vh' }}> */}
-                <Featured projectImgs={featuredImgData} />
-                <Projects projectImgs={projectImgData} />
-                {/* </div> */}
-                <Contact href='#contact' />
-            </SectionStyles>
-        </Layout>
-    )
+      <ArticleStyles>
+        <BasicInfo />
+        <Featured projectImgs={featuredImgData} />
+        <Projects projectImgs={projectImgData} />
+        <Contact href='#contact' />
+      </ArticleStyles>
+    </Layout>
+  )
 }
 
 export default IndexPage
 
 export const query = graphql`
-    query allDataQuery {
-        site {
-            siteMetadata {
-                title
-                description
-            }
-        }
-
-        ProjectImgs: allFile(
-            sort: { order: ASC, fields: [absolutePath] }
-            filter: { relativePath: { regex: "/projects/.*.png/" } }
-        ) {
-            edges {
-                node {
-                    relativePath
-                    name
-                    childImageSharp {
-                        sizes(maxWidth: 320) {
-                            ...GatsbyImageSharpSizes
-                        }
-                    }
-                }
-            }
-        }
-
-        FeaturedProjectImgs: allFile(
-            sort: { order: ASC, fields: [absolutePath] }
-            filter: { relativePath: { regex: "/projects/.*.png/" } }
-        ) {
-            edges {
-                node {
-                    relativePath
-                    name
-                    childImageSharp {
-                        sizes(maxWidth: 600) {
-                            ...GatsbyImageSharpSizes
-                        }
-                    }
-                }
-            }
-        }
+  query allDataQuery {
+    site {
+      siteMetadata {
+        title
+        description
+      }
     }
+
+    ProjectImgs: allFile(
+      sort: { order: ASC, fields: [absolutePath] }
+      filter: { relativePath: { regex: "/projects/.*.png/" } }
+    ) {
+      edges {
+        node {
+          relativePath
+          name
+          childImageSharp {
+            sizes(maxWidth: 320) {
+              ...GatsbyImageSharpSizes
+            }
+          }
+        }
+      }
+    }
+
+    FeaturedProjectImgs: allFile(
+      sort: { order: ASC, fields: [absolutePath] }
+      filter: { relativePath: { regex: "/projects/.*.png/" } }
+    ) {
+      edges {
+        node {
+          relativePath
+          name
+          childImageSharp {
+            sizes(maxWidth: 600) {
+              ...GatsbyImageSharpSizes
+            }
+          }
+        }
+      }
+    }
+  }
 `
