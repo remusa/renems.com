dfe37ba3948a7f98121e2a6edfc729512f8bab78
R Sánchez <rene.as.himself@gmail.com>
2019-11-29 17:33:59 -0600

    Added SVG icons

---
+++ src/static/icons/index.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const email = './email-outline.svg'
+const github = './github-outline.svg'
+const linkedin = './linkedin-outline.svg'
+const twitter = './twitter-outline.svg'
+const video = './video-outline.svg'
+const sunFilled = './sun-fill.svg'
+const sunOutline = './sun-outline.svg'
+const moonFilled = './moon-fill.svg'
+const moonOutline = './moon-outline.svg'
+
+const IconEmail = () => email
+
+export {
+  IconEmail,
+  email,
+github,
+linkedin,
+twitter,
+video,
+sunFilled,
+sunOutline,
+moonFilled,
+moonOutline,
+}
