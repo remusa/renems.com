6a6ceef30ca0b24a0a840299a1eb8d47f685793f
R Sánchez <rene.as.himself@gmail.com>
2019-11-29 19:19:06 -0600

    CSS transitions

--- src/components/social.js
+++ src/components/social.js
@@ -4,97 +4,102 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {
   faGithub,
   faLinkedin,
-  // faYoutube,
-  // faTwitter,
+  faTwitter,
+  faYoutube,
 } from '@fortawesome/free-brands-svg-icons'
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
 // import socialLinks from '../data/social.json'
-import AnchorLink from 'react-anchor-link-smooth-scroll'
+// import AnchorLink from 'react-anchor-link-smooth-scroll'
 
 const SocialStyles = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
 
-  .social-container {
-    background: #eee;
-    padding: 1.5rem 2.5rem;
-  }
+  .social-link {
+    display: flex;
+    justify-content: center;
+    align-items: center;
 
-  a.social {
     color: var(--color-primary-light);
     margin: 0 1rem;
     transition: transform 0.3s;
     display: inline-block;
-    size: 25px;
+    text-align: center;
+
+    .icon-title {
+      line-height: 0;
+      opacity: 0;
+      font-weight: bold;
+      font-size: 1rem;
+    }
 
     &:hover {
       /* transform: translateY(-2px); */
       transform: translateZ(0) scale(0.85);
       color: var(--color-primary);
-      opacity: 1;
+
+      .icon-title {
+        opacity: 1;
+        transform: 0.3s translate3d(0, 0.5rem, 0);
+        transition: 0.3s cubic-bezier(0.75, 0, 0.08, 1);
+      }
     }
   }
-
-  /* a.youtube {
-    color: #eb3223;
-  }
-
-  a.github {
-    color: #24292e;
-  }
-
-  a.twitter {
-    color: #49a1eb;
-  }
-
-  a.linkedin {
-    color: #0073b0;
-  }
-
-  a.email {
-    color: #333333;
-  } */
 `
 
 const Social = () => {
   const iconSize = '1x'
 
   return (
-    <SocialStyles className='social-container'>
+    <SocialStyles>
       <a
         target='_blank'
         rel='noopener noreferrer'
         href='https://github.com/remusa'
-        className='social github'
+        className='social-link github'
       >
         <FontAwesomeIcon icon={faGithub} size={iconSize} />
+        <p className="icon-title">Github</p>
       </a>
 
       <a
         target='_blank'
         rel='noopener noreferrer'
         href='https://www.linkedin.com/in/remusa'
-        className='social linkedin'
+        className='social-link linkedin'
       >
         <FontAwesomeIcon icon={faLinkedin} size={iconSize} />
+        <p className="icon-title">LinkedIn</p>
       </a>
 
-      {/* <AnchorLink href="#contact" className='social email'>
+      {/* <AnchorLink href="#contact" className='social-link email'>
         <FontAwesomeIcon icon={faEnvelope} size={iconSize} />
       </AnchorLink> */}
 
-      <a href='#contact' className='social email'>
+      <a href='#contact' className='social-link email'>
         <FontAwesomeIcon icon={faEnvelope} size={iconSize} />
+        <p className="icon-title">Email</p>
       </a>
 
       {/* <a
         target='_blank'
         rel='noopener noreferrer'
         href='https://twitter.com/remusa_'
-        className='social twitter'
+        className='social-link twitter'
       >
         <FontAwesomeIcon icon={faTwitter} size={iconSize} />
+        <p className="icon-title">Twitter</p>
+      </a> */}
+
+      {/* <a
+        target='_blank'
+        rel='noopener noreferrer'
+        href='https:/youtube.com/remusa_'
+        className='social-link twitter'
+      >
+        <FontAwesomeIcon icon={faYoutube} size={iconSize} />
+        <p className="icon-title">YouTube</p>
       </a> */}
     </SocialStyles>
   )
