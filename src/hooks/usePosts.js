7604119a036ff20672af7b78a0319356d83dcf4e
R Sánchez <rene.as.himself@gmail.com>
2020-01-25 23:45:31 -0600

    Create usePosts.js

---
+++ src/hooks/usePosts.js
@@ -0,0 +1,24 @@
+const usePosts = () => {
+  // // const data = useStaticQuery(graphql`
+  // //   query {
+  //     # allMdx {
+  //     #   nodes {
+  //     #     frontmatter {
+  //     #       title
+  //     #       slug
+  //     #       author
+  //     #     }
+  //     #     excerpt
+  //     #   }
+  //     # }
+  //   # }
+  // // `)
+  // return data.allMdx.nodes.map(post => {
+  //   title: post.frontmatter.title,
+  //   author: post.frontmatter.author,
+  //   slug: post.frontmatter.slug,
+  //   excerpt: post.excerpt,
+  // })
+}
+
+export default usePosts
