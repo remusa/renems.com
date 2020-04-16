ec5355b87d1ee3e83474b39b23144c88d787f26f
R Sánchez <rene.as.himself@gmail.com>
2020-04-16 17:47:51 -0500

    fix: updated dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,9 @@
-const prettierConfig = require('./.prettierrc')
+const prettierConfig = require('./.prettierrc.js')
 
 module.exports = {
-  extends: ['wesbos'],
+  "extends": ["react-app"],
+  // "extends": ["react-app", "plugin:jsx-a11y/recommended"],
+  // "plugins": ["jsx-a11y"],
   rules: {
     'prettier/prettier': ['error', prettierConfig],
   },
