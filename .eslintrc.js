1d3a4c1e6560525595b7d33c35cb345ac814038d
R Sánchez <rene.as.himself@gmail.com>
2020-04-21 17:14:08 -0500

    Fixed husky and lint-staged

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,6 +5,9 @@ module.exports = {
   // "extends": ["react-app", "plugin:jsx-a11y/recommended", "plugin:prettier/recommended"],
   // "plugins": ["jsx-a11y"],
   plugins: ['prettier'],
+  env: {
+    browser: true,
+  },
   rules: {
     'babel/camelcase': 'off',
     'babel/quotes': 'off',
@@ -15,6 +18,21 @@ module.exports = {
     'max-lines-per-function': 'off',
     'react/jsx-pascal-case': 'off',
     'react/prop-types': 'off',
-    'prettier/prettier': ['error', prettierConfig],
+    // 'prettier/prettier': ['error', prettierConfig],
   },
+  overrides: [
+    {
+      files: '**/*.+(ts|tsx)',
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        project: './tsconfig.json',
+      },
+      plugins: ['@typescript-eslint/eslint-plugin'],
+      extends: [
+        'plugin:@typescript-eslint/eslint-recommended',
+        'plugin:@typescript-eslint/recommended',
+        'eslint-config-prettier/@typescript-eslint',
+      ],
+    },
+  ],
 }
