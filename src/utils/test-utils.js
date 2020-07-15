0fac89db0c5f4527f49df9d88a42de5a2f0ac29f
R Sánchez <rene.as.himself@gmail.com>
2020-07-15 17:25:59 -0500

    Fixed Jest and RTL integration

--- src/utils/test-utils.js
+++ src/utils/test-utils.js
@@ -1,6 +1,7 @@
 // test-utils.js
 import React from 'react'
 import { render } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 
 const AllTheProviders = ({ children }) => {
   return <>{children}</>
@@ -12,4 +13,4 @@ const customRender = (ui, options) => render(ui, { wrapper: AllTheProviders, ...
 export * from '@testing-library/react'
 
 // override render method
-export { customRender as render }
+export { customRender as render, userEvent }
