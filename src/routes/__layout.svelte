70309ea7f0d285dd8ae655b9c1ad65d517ef1494
R Sánchez <9169831+remusa@users.noreply.github.com>
2022-04-14 15:43:54 -0500

    feat: add page transitions

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,15 +1,32 @@
+<script context="module">
+  export const load = ({ url }) => {
+    const currentRoute = url.pathname
+
+    return {
+      props: {
+        currentRoute,
+      },
+    }
+  }
+</script>
+
 <script>
   import Footer from '$lib/components/Footer.svelte'
   import Header from '$lib/components/Header.svelte'
   import '$lib/styles/style.css'
+  import { fade } from 'svelte/transition'
+
+  export let currentRoute
 </script>
 
 <div class="">
   <Header />
 
-  <main>
-    <slot />
-  </main>
+  {#key currentRoute}
+    <main in:fade={{ duration: 150, delay: 150 }} out:fade={{ duration: 150 }}>
+      <slot />
+    </main>
+  {/key}
 
   <Footer />
 </div>
--- src/routes/blog/_post.svelte
+++ src/routes/blog/_post.svelte
@@ -52,4 +52,6 @@
   </aside>
 {/if}
 
+<hr />
+
 <slot />
