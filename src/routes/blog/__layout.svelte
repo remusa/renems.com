1654512332fde98f4d0c9bdf6c9be160f53d167a
R Sánchez <9169831+remusa@users.noreply.github.com>
2022-04-14 13:59:17 -0500

    feat: add blog post layout

--- src/routes/blog/__layout.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-  import Footer from '$lib/components/Footer.svelte'
-  import Header from '$lib/components/Header.svelte'
-  import '$lib/styles/themes/prism-one-dark.css'
-</script>
-
-<div>
-  <slot />
-</div>
---
+++ src/routes/blog/_post.svelte
@@ -0,0 +1,23 @@
+<script>
+  import '$lib/styles/themes/prism-one-dark.css'
+  export let date = '2020-07-15'
+  export let title = 'MDX Test'
+  export let path = '/test-mdx'
+  export let author = 'RMS'
+  export let tags = ['mdx']
+  export let language = 'english'
+  export let type = 'BLOG'
+  export let published = false
+</script>
+
+<h1>Title: {title}</h1>
+
+<p>Published: {date}</p>
+<p>Path: {path}</p>
+<p>Author: {author}</p>
+<p>Tags: {tags}</p>
+<p>Language: {language}</p>
+<p>Type: {type}</p>
+<p>Published: {published}</p>
+
+<slot />
