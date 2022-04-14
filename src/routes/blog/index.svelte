59a474a63ea940820caf4e89af9ae025abdd6322
R Sánchez <9169831+remusa@users.noreply.github.com>
2022-04-14 14:32:26 -0500

    feat: show posts in blog index

--- src/routes/blog/index.svelte
+++ src/routes/blog/index.svelte
@@ -1,9 +1,35 @@
+<script context="module">
+  export async function load({ fetch }) {
+    const posts = await fetch('/api/posts.json')
+    const allPosts = await posts.json()
+    console.log('allPosts', allPosts)
+
+    return {
+      props: {
+        posts: allPosts,
+      },
+    }
+  }
+</script>
+
 <script>
+  export let posts
 </script>
 
 <h1>Blog</h1>
 
 <h2>Blog Posts</h2>
+
+{#each posts as post}
+  <li>
+    <h2>
+      <a href={post.path}>
+        {post.meta.title}
+      </a>
+    </h2>
+    Published {post.meta.date}
+  </li>
+{/each}
 
 <style>
 </style>
