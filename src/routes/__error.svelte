819d651440310d4272acda5745b92f01aeec5223
R Sánchez <9169831+remusa@users.noreply.github.com>
2022-04-14 17:49:25 -0500

    feat: add error page

---
+++ src/routes/__error.svelte
@@ -0,0 +1,24 @@
+<script context="module">
+  export async function load({ error, status }) {
+    return {
+      props: {
+        error,
+        status,
+      },
+    }
+  }
+</script>
+
+<script>
+  export let status
+  export let error
+</script>
+
+<h2>{status}</h2>
+
+<p class="subhead">{error.message}</p>
+
+<p><strong>Error!</strong> Try these links</p>
+<ul>
+  <li><a href="/">Home</a></li>
+</ul>
