9ab056e509ed39cf8255c217270277d2ce478f70
R Sánchez <9169831+remusa@users.noreply.github.com>
2023-01-28 21:17:30 -0600

    feat: add svelte component

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,10 @@ import sitemap from '@astrojs/sitemap'
 import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
+import svelte from '@astrojs/svelte'
+
+// https://astro.build/config
 export default defineConfig({
   site: 'https://renems.com',
-  integrations: [mdx(), sitemap()],
+  integrations: [mdx(), sitemap(), svelte()],
 })
---
+++ src/components/Greeting.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  export let messages: string[] = []
+  let greeting: string = messages.at(0)
+
+  function randomMessage() {
+    return messages[Math.floor(Math.random() * messages.length)]
+  }
+
+  function onClick() {
+    greeting = randomMessage()
+  }
+</script>
+
+<div>
+  <h3>{greeting}! Thank you for visiting!</h3>
+  <button on:click={onClick}> New Greeting </button>
+</div>
+
+<style>
+</style>
