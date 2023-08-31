ed726d10ce30f12a1b4058ee3d684d646e2db1da
R Sánchez <9169831+remusa@users.noreply.github.com>
2023-08-30 21:43:37 -0600

    chore: use tailwind

--- astro.config.js
+++ astro.config.js
@@ -6,16 +6,18 @@ import { defineConfig } from 'astro/config'
 import svelte from '@astrojs/svelte'
 
 // https://github.com/unocss/unocss
-import presetIcons from '@unocss/preset-icons'
-import presetUno from '@unocss/preset-uno'
-import presetWebFonts from '@unocss/preset-web-fonts'
-import UnoCSS from 'unocss/astro'
-
+// import presetIcons from '@unocss/preset-icons'
+// import presetUno from '@unocss/preset-uno'
+// import presetWebFonts from '@unocss/preset-web-fonts'
+// import UnoCSS from 'unocss/astro'
+import tailwind from '@astrojs/tailwind'
 const root = {
   colorBackground: '#282c34',
   colorPrimary: '#2bbac5',
   colorSecondary: '#ef596f',
 }
+
+// https://astro.build/config
 
 // https://astro.build/config
 export default defineConfig({
@@ -24,27 +26,28 @@ export default defineConfig({
     mdx(),
     sitemap(),
     svelte(),
-    UnoCSS({
-      presets: [
-        presetIcons({}),
-        presetUno(),
-        presetWebFonts({
-          provider: 'google', // default provider
-          fonts: {
-            // these will extend the default theme
-            sans: 'Inter',
-            mono: ['Fira Code', 'Fira Mono:400,700'],
-          },
-        }),
-      ],
-      // https://github.com/unocss/unocss#extend-theme
-      theme: {
-        colors: {
-          background: root.colorBackground,
-          primary: root.colorPrimary,
-          secondary: root.colorSecondary,
-        },
-      },
-    }),
+    // UnoCSS({
+    //   presets: [
+    //     presetIcons({}),
+    //     presetUno(),
+    //     presetWebFonts({
+    //       provider: 'google', // default provider
+    //       fonts: {
+    //         // these will extend the default theme
+    //         sans: 'Inter',
+    //         mono: ['Fira Code', 'Fira Mono:400,700'],
+    //       },
+    //     }),
+    //   ],
+    //   // https://github.com/unocss/unocss#extend-theme
+    //   theme: {
+    //     colors: {
+    //       background: root.colorBackground,
+    //       primary: root.colorPrimary,
+    //       secondary: root.colorSecondary,
+    //     },
+    //   },
+    // }),
+    tailwind(),
   ],
 })
