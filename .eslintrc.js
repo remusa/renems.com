b38cfcb944367b1ac0975c49f955a36081fa967d
R Sánchez <9169831+remusa@users.noreply.github.com>
2023-11-15 20:28:22 -0600

    chore: use astropaper template

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,55 +1,23 @@
-const prettierConfig = require('./.prettierrc')
-
 module.exports = {
-  extends: [
-    'react-app',
-    //"plugin:jsx-a11y/recommended",
-    'plugin:prettier/recommended',
-    'plugin:jest-dom/recommended',
-    'plugin:testing-library/recommended',
-    'plugin:testing-library/react',
-  ],
-  plugins: [
-    'prettier',
-    //"jsx-a11y",
-    'testing-library',
-    'jest-dom',
-  ],
   env: {
+    node: true,
+    es2022: true,
     browser: true,
   },
-  rules: {
-    'babel/camelcase': 'off',
-    'babel/quotes': 'off',
-    'consistent-return': 'off',
-    'import/no-unresolved': 'off',
-    'import/order': 'off',
-    'jsx-a11y/label-has-for': 'off',
-    'max-lines-per-function': 'off',
-    'react/jsx-pascal-case': 'off',
-    'react/prop-types': 'off',
-    // 'prettier/prettier': ['error', prettierConfig],
-    'jest-dom/prefer-checked': 'error',
-    'jest-dom/prefer-enabled-disabled': 'error',
-    'jest-dom/prefer-required': 'error',
-    'jest-dom/prefer-to-have-attribute': 'error',
-    'testing-library/await-async-query': 'error',
-    'testing-library/no-await-sync-query': 'error',
-    'testing-library/no-debug': 'warn',
+  extends: ['eslint:recommended', 'plugin:astro/recommended'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
   },
   overrides: [
     {
-      files: '**/*.+(ts|tsx)',
-      parser: '@typescript-eslint/parser',
+      files: ['*.astro'],
+      parser: 'astro-eslint-parser',
       parserOptions: {
-        project: './tsconfig.json',
+        parser: '@typescript-eslint/parser',
+        extraFileExtensions: ['.astro'],
       },
-      plugins: ['@typescript-eslint/eslint-plugin'],
-      extends: [
-        'plugin:@typescript-eslint/eslint-recommended',
-        'plugin:@typescript-eslint/recommended',
-        'eslint-config-prettier/@typescript-eslint',
-      ],
+      rules: {},
     },
   ],
 }
