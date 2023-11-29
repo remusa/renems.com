6f3f1e593241ac4ba9d235e5662fdd13a72b97fc
R Sánchez <9169831+remusa@users.noreply.github.com>
2023-11-28 21:11:45 -0600

    chore: style underline

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -13,14 +13,14 @@ export default function Card({ href, frontmatter, secHeading = true }: Props) {
 
   const headerProps = {
     style: { viewTransitionName: slugifyStr(title) },
-    className: 'text-lg font-medium decoration-dashed hover:underline',
+    className: 'text-lg font-medium decoration-slice hover:underline',
   }
 
   return (
     <li className='my-6'>
       <a
         href={href}
-        className='inline-block text-lg font-medium text-skin-accent decoration-dashed underline-offset-4 focus-visible:no-underline focus-visible:underline-offset-0'
+        className='inline-block decoration-slice text-lg font-medium text-skin-accent underline-offset-4 focus-visible:no-underline focus-visible:underline-offset-0'
       >
         {secHeading ? (
           <h2 {...headerProps}>{title}</h2>
